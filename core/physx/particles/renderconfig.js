ebba0699989cd81e2fe8356eb40ef037b72b5bad
inwebo <inwebo@gmail.com>
2015-02-23 14:13:59 +0100

    Fix namespace

--- core/physx/particles/renderconfig.js
+++ core/physx/particles/renderconfig.js
@@ -3,9 +3,8 @@
     var L        = window.LibreJs = window.LibreJs || {};
     var Plugins  = L.Plugins      = L.Plugins      || {};
     var Physx  = Plugins.Physx = Plugins.Physx || {};
-    var Water  = Plugins.Physx.Water = Plugins.Physx.Water   || {};
-
-    Water.RenderConfig = function(){
+    var Particles  = Plugins.Physx.Particles = Plugins.Physx.Particles   || {};
+    Particles.RenderConfig = function(){
         var plugin              = this;
         plugin.particle.width   = 100;
     };
