c842595ac50cb5cd10fc2cc67b6950e8b9f3beeb
inwebo <inwebo@gmail.com>
2015-02-28 00:33:40 +0100

    Add particule sprite support

--- core/physx/particles/render.js
+++ core/physx/particles/render.js
@@ -5,12 +5,14 @@
     var Physx  = Plugins.Physx = Plugins.Physx || {};
     var Particles  = Plugins.Physx.Particles = Plugins.Physx.Particles   || {};
 
-    Particles.Render          = function(canvas, simulation){
+    Particles.Render          = function(canvas, simulation, sprite,fps){
         var plugin = this;
 
         plugin.canvas;
         plugin.ctx;
         plugin.simulation;
+        plugin.sprite = sprite || null;
+        plugin.fps = fps || 1000 / 60;
 
         var init = function(canvas,simulation){
             plugin.canvas = canvas;
@@ -30,17 +32,32 @@
         };
 
         plugin.draw = function(){
+
             plugin.clear();
             var l = plugin.simulation.particles.length;
             for(var i = 0 ; i < l ; i++) {
                 var particle = plugin.simulation.particles[i];
-                particle.draw(plugin.ctx);
+                if( plugin.sprite !== null) {
+                    particle.draw(plugin.ctx, plugin.sprite);
+                }
+                else {
+                    particle.draw(plugin.ctx);
+                }
+
             };
+
             var l = plugin.simulation.dampers.length;
             for(var i = 0 ; i < l ; i++) {
                 var damper = plugin.simulation.dampers[i];
                 damper.draw(plugin.ctx);
             };
+        };
+
+        plugin.attachParticleSprite = function(src){
+            var i = new Image();
+            i.src = src;
+            plugin.sprite = i;
+
         };
 
         init(canvas,simulation);
