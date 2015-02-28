463404d02ed100ae93bfe7aac532004a84edef57
inwebo <inwebo@gmail.com>
2015-02-28 16:45:27 +0100

    Add tail helper

---
+++ core/physx/particles/tail.js
@@ -0,0 +1,15 @@
+//<![CDATA[
+;(function(window){
+    window.LibreJs.Plugins.Physx.Particles.Tail = {
+
+        /**
+         * Retourne une particule static et sans tail.
+         */
+        factory : function(){
+
+        }
+
+    };
+    var Vector      = window.LibreJs.Plugins.Physx.Particles.Vector.prototype.constructor;
+})(window);
+//]]>
