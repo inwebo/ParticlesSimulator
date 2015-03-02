5b0316dd7ad2aeb9bc22dadcdb0078b8b7576d85
inwebo <inwebo@gmail.com>
2015-03-02 22:35:33 +0100

    Step

--- core/physx/particles/header.js
+++ core/physx/particles/header.js
@@ -3,4 +3,5 @@ window.LibreJs = window.LibreJs || {};
 window.LibreJs.Plugins = window.LibreJs.Plugins || {};
 window.LibreJs.Plugins.Physx = window.LibreJs.Plugins.Physx || {};
 window.LibreJs.Plugins.Physx.Particles = window.LibreJs.Plugins.Physx.Particles || {};
+window.LibreJs.Plugins.Physx.Particles.Simulation = window.LibreJs.Plugins.Physx.Particles.Simulation || {};
 //]]>
