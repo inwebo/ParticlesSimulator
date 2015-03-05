0b2cdf2ab21eef75bc9013a68ce6f1f8ebdefdfc
inwebo <inwebo@gmail.com>
2015-03-05 18:08:05 +0100

    Ultra simple Assert test

--- plugins/assert.js
+++ plugins/assert.js
@@ -7,3 +7,12 @@ function assert(condition, message) {
         throw message; // Fallback
     }
 }
+
+function getCurrentFile(){
+    var scripts = document.getElementsByTagName("script");
+    return scripts[scripts.length-1].getAttribute('src');
+};
+
+var Assert = function(condition){
+    return getCurrentFile();
+};
