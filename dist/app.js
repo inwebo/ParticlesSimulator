28e7f2a3757abc22df259c9a8fb7619f64fb50c8
inwebo <gh_kvrg1hir!>
2020-01-23 23:54:04 +0100

    step

--- dist/app.js
+++ dist/app.js
@@ -7,91 +7,27 @@
 // import Damper from "../src/Damper/Damper";
 // import BaseRender from "../src/Render/BaseRender";
 
-import Render2D from "../src/Render/Render2D";
-import Vector from "@inwebo/vector";
+import RenderVector from "../src/Render/RenderVector";
+import Vector2D from "@inwebo/vector/src/Vector2D";
 
 document.addEventListener("DOMContentLoaded",function() {
-  // const bounds     = new Bounds(new Vector(0,0), new Vector(600,600));
-  // const simulation = new Simulation(bounds);
-  // const canvas = document.getElementById('demo');
-  // const render = new Render(canvas, simulation);
-  //
-  // const emitter = new Emitter(
-  //   new Vector(300, 300),
-  //   new Vector(2, 2),
-  //   500,
-  //   0,
-  //   -1
-  // );
-  //
-  // const damper = new Damper(
-  //   new Vector(300, 450),
-  //   10
-  // );
-  //
-  // simulation.pushEmitter(emitter);
-  // simulation.pushDamper(damper);
+  const layer0 = document.getElementById('layer0');
+  const render = new RenderVector(layer0);
+  layer0.addEventListener('mousemove', function(event) {
+    if(event.test) {
+      alert('ouch, my eye :(');
+    }
+  });
+  const vector = new Vector2D(100, 0);
 
-  // const draw = function() {
-  //   setTimeout(() => {
-  //     const now = Date.now();
-  //     simulation.step(now);
-  //     render.draw();
-  //     requestAnimationFrame(draw);
-  //   },10);
-  // };
+  // console.log(vector);
 
-  const canvas = document.getElementById('demo');
-  const render = new Render2D(canvas);
-
-  const draw = function() {
+  const draw = () => {
     setTimeout(() => {
-      render.draw();
+      render.draw(vector);
       requestAnimationFrame(draw);
     },10);
   };
 
-  // simulation.step(Date.now());
-  // simulation.step(Date.now() + 1000000000000);
-  // render.draw();
-
   requestAnimationFrame(draw);
-
-
-  // const bounds     = new Bounds(new Vector(0,0), new Vector(600,600));
-  // const simulation = new Simulation(bounds);
-  // const canvas = document.getElementById('demo');
-  // const render = new Render(canvas, simulation);
-  //
-  // const emitter = new Emitter(
-  //     new Vector(300, 300),
-  //     new Vector(0, 10),
-  //     500,
-  //     0,
-  //     -1
-  // );
-  //
-  // simulation.pushEmitter(emitter);
-  //
-  // const draw = function(timestamp) {
-  //     setTimeout(function() {
-  //         simulation.step(timestamp);
-  //         render.draw();
-  //         requestAnimationFrame(draw);
-  //     },60);
-  //     // console.table(simulation);
-  // };
-  // requestAnimationFrame(draw);
-
-  // const ticker = new Ticker(1000);
-  //
-  // const draw = () => {
-  //     setTimeout(function() {
-  //         const now = Date.now();
-  //         ticker.step(now);
-  //         requestAnimationFrame(draw);
-  //     },60);
-  // };
-  //
-  // requestAnimationFrame(draw);
 });
