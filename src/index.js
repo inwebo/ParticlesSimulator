915bf6a39cc0fcf4f7e8eab52fa277257c268f1d
inwebo <gh_kvrg1hir!>
2020-01-23 23:55:08 +0100

    tests

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import Damper from "./Damper/Damper";
 import Emitter from "./Emitters/Emitter";
-import MathExtension from "./MathExtension/MathExtension";
 import Particle from "./Particle/Particle";
-import Render from "./Render/Render";
 import Bounds from "./Simulation/Bounds";
 import Simulation from "./Simulation/Simulation";
 import Ticker from "./Ticker/Ticker";
-import Vector from "./Vector/Vector";
-
-import Vector2D from "@inwebo/vector";
+import BaseRender from "./Render/BaseRender";
+import { Vector2D } from "@inwebo/vector/src/Vector2D";
