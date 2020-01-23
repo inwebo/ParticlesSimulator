ba74da3ea7712d1bf5788faa6e988515846b390c
inwebo <gh_kvrg1hir!>
2020-01-23 22:29:24 +0100

    step

--- dist/app.js
+++ dist/app.js
@@ -1,37 +1,51 @@
-import Simulation from "../src/Simulation/Simulation";
-import Bounds from "../src/Simulation/Bounds";
-import Vector from "../src/Vector/Vector";
-import Render from "../src/Render/Render";
-import Emitter from "../src/Emitters/Emitter";
-import Ticker from "../src/Ticker/Ticker";
-import Damper from "../src/Damper/Damper";
+// import Simulation from "../src/Simulation/Simulation";
+// import Bounds from "../src/Simulation/Bounds";
+// import Vector from "../src/Vector/Vector";
+// import Render from "../src/Render/Render";
+// import Emitter from "../src/Emitters/Emitter";
+// import Ticker from "../src/Ticker/Ticker";
+// import Damper from "../src/Damper/Damper";
+// import BaseRender from "../src/Render/BaseRender";
+
+import Render2D from "../src/Render/Render2D";
+import Vector from "@inwebo/vector";
 
 document.addEventListener("DOMContentLoaded",function() {
-  const bounds     = new Bounds(new Vector(0,0), new Vector(600,600));
-  const simulation = new Simulation(bounds);
+  // const bounds     = new Bounds(new Vector(0,0), new Vector(600,600));
+  // const simulation = new Simulation(bounds);
+  // const canvas = document.getElementById('demo');
+  // const render = new Render(canvas, simulation);
+  //
+  // const emitter = new Emitter(
+  //   new Vector(300, 300),
+  //   new Vector(2, 2),
+  //   500,
+  //   0,
+  //   -1
+  // );
+  //
+  // const damper = new Damper(
+  //   new Vector(300, 450),
+  //   10
+  // );
+  //
+  // simulation.pushEmitter(emitter);
+  // simulation.pushDamper(damper);
+
+  // const draw = function() {
+  //   setTimeout(() => {
+  //     const now = Date.now();
+  //     simulation.step(now);
+  //     render.draw();
+  //     requestAnimationFrame(draw);
+  //   },10);
+  // };
+
   const canvas = document.getElementById('demo');
-  const render = new Render(canvas, simulation);
-
-  const emitter = new Emitter(
-    new Vector(300, 300),
-    new Vector(2, 2),
-    500,
-    0,
-    -1
-  );
-
-  const damper = new Damper(
-    new Vector(300, 450),
-    10
-  );
-
-  simulation.pushEmitter(emitter);
-  simulation.pushDamper(damper);
+  const render = new Render2D(canvas);
 
   const draw = function() {
     setTimeout(() => {
-      const now = Date.now();
-      simulation.step(now);
       render.draw();
       requestAnimationFrame(draw);
     },10);
