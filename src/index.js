31fc2250818815e05bda0b865b70f1c855e5a95f
Inwebo <github@inwebo.net>
2020-01-25 16:22:42 +0100

    Add new renderer

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ import Particle from "./Particle/Particle";
 import BaseRender from "./Render/BaseRender";
 import Render2D from "./Render/Render2D";
 import RenderVector from "./Render/RenderVector";
+import RenderParticle from "./Render/RenderParticle";
