855b6e7169ad86f981c6b0035f73ec950db44a43
Inwebo <github@inwebo.net>
2020-01-25 16:35:50 +0100

    Add getters

--- src/Particle/Particle.js
+++ src/Particle/Particle.js
@@ -3,6 +3,20 @@ import Vector2D from "@inwebo/vector/src/Vector2D";
 export default class Particle {
 
     /**
+     * @return {Vector2D}
+     */
+    getPosition()  {
+        return this._position;
+    }
+
+    /**
+     * @return {Vector2D}
+     */
+    getVelocity()  {
+        return this._velocity;
+    }
+
+    /**
      * @param {Vector2D|null} position
      * @param {Vector2D|null} velocity
      * @param {Vector2D|null} acceleration
