3f63b90b65e25a78c2e60c97f26cd19c2c316b18
Inwebo <github@inwebo.net>
2020-01-25 16:41:59 +0100

    Fix path

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import Bounds from "./Bounds/Bounds";
 import Particle from "./Particle/Particle";
 import BaseRender from "./Render/BaseRender";
 import Render2D from "./Render/Render2D";
+import RenderFps from "./Render/RenderFps";
 import RenderVector from "./Render/RenderVector";
 import RenderParticle from "./Render/RenderParticle";
