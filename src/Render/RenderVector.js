7c2556248e1d82f206d094ae8ae10fd473af504e
Inwebo <github@inwebo.net>
2020-01-25 16:36:12 +0100

    Add _clear()

--- src/Render/RenderVector.js
+++ src/Render/RenderVector.js
@@ -4,20 +4,28 @@ import Vector2D from "@inwebo/vector/src/Vector2D";
 export default class RenderVector extends Render2D {
 
     _setOrigin() {
-        this.setOriginPosition(this._position.ORIGIN_BOTTOM_LEFT);
+        // this.setOriginPosition(this._position.ORIGIN_CENTER);
+    }
+
+    _clear(...subject) {
+        this.getCtx().fillStyle = 'rgba(255,255,255,0.3)';
+        this.getCtx().fillRect(0,0,this._canvas.width,this._canvas.height);
     }
 
     /**
      * Must be overrided
-     * @param {[*]} subject
+     * @param {[Vector2D]} subject
      * @protected
      */
     _draw(...subject) {
-        const particle = Vector2D.fromObject(subject[0]);
+        /**
+         * @type {Vector2D}
+         */
+        const particle = subject[0][0];
 
         this.getCtx().beginPath();
-        this.getCtx().arc(particle.getX(), particle.getY(), 10, 0, 2 * Math.PI, false);
-        this.getCtx().fillStyle = `rgba(0,0,0,0.3}`;
+        this.getCtx().fillStyle ='rgba(0,0,0,1)';
+        this.getCtx().arc(particle.getX(), particle.getY(), 50, 0, 2 * Math.PI, false);
         this.getCtx().fill();
     }
 }
