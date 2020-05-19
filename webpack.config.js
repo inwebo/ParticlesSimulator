889569b0f173c1ad47d1f7def63b18615f487cee
inwebo <github@inwebo.net>
2020-05-19 20:12:25 +0200

    Update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,13 @@ const path = require('path');
 
 module.exports = {
     entry: {
-        simulator: './src/index.js',
-        app: './dist/app.js',
+        main: './src/index.js',
+        app: './docs/assets/js/app.js',
     },
     output: {
-        filename: '[name].min.js',
-        path: path.join(__dirname, 'dist'),
+        filename: (pathData) => {
+            return (pathData.chunk.name === 'app') ? 'docs/assets/js/[name].min.js' : 'dist/[name].min.js';
+        },
+        path: path.join(__dirname, './'),
     }
 };
